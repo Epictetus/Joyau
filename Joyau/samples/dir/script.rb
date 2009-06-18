a673b4b73df857c1fe941ccc420eb044e1c8533d
kilianverhetsel@yahoo.fr <kilian@arch-desktop.localdomain>
2009-06-18 20:06:28 +0200

    minor modification to the dir sample

--- Joyau/samples/dir/script.rb
+++ Joyau/samples/dir/script.rb
@@ -1,14 +1,18 @@
 initLib
 initGfx
 
+puts "would start"
 dir = PSPDir.new
 dir.setDir("ms0:/PSP/GAME/")
 array = dir.ls
 
 while mayPlay
   startDraw
+  y = 0
   for var in array
     puts var.to_s
+    #drawText(0, y, var.to_s)
+    y += 20
   end
   endDraw
 
