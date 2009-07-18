f5cbe03ed5fde973aba59df4cad553080c9ace6f
Kilian Verhetsel <kilianverhetsel@yahoo.fr>
2009-07-18 14:41:23 +0200

    removing references from GameMap ( it caused bug )

--- Joyau/GameMap.cpp
+++ Joyau/GameMap.cpp
@@ -1,18 +1,19 @@
+
 /*Copyright (C) 2009 Verhetsel Kilian
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.*/
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.*/
 
 #include "GameMap.hpp"
 
@@ -62,22 +63,22 @@ void GameMap::addElem(int tileset, int tX, int tY, int x, int y)
    t.tileY = tY;
    t.x = x;
    t.y = y;
-   
+
    tiles.push_back(t);
 }
 
-bool GameMap::collide(Sprite &spr)
+bool GameMap::collide(Sprite *spr)
 {
    for (list<Tile>::iterator i = tiles.begin(); i != tiles.end(); ++i)
    {
       Sprite &tile = tilesets[(*i).tileset];
       tile.setTile((*i).tileX, (*i).tileY, tileWidth, tileHeight);
       tile.setPos(_x + (*i).x, _y + (*i).y); // Coord relative to the map !
-      
+
       tile.getImage(); // We update informations
-      
-      if (tile.collide(spr))
-	 return true; // If it collides with only one tile, it collides..
+
+      if (tile.collide(*spr))
+         return true; // If it collides with only one tile, it collides..
    }
    return false; // It didn't collide with any sprite.
 }
@@ -89,11 +90,11 @@ bool GameMap::isOn(int x, int y)
       Sprite &tile = tilesets[(*i).tileset];
       tile.setTile((*i).tileX, (*i).tileY, tileWidth, tileHeight);
       tile.setPos(_x + (*i).x, _y + (*i).y);
-      
+
       tile.getImage();
-      
+
       if (tile.isOn(x, y))
-	 return true;
+         return true;
    }
    return false;
 }
@@ -104,7 +105,7 @@ void GameMap::clear()
    tilesets.clear();
 }
 
-bool GameMap::visible(Tile &t)
+bool GameMap::visible(Tile t)
 {
    int x = t.x + _x;
    int y = t.y + _y;
@@ -128,141 +129,140 @@ void GameMap::draw()
    for (list<Tile>::iterator i = tiles.begin(); i != tiles.end(); ++i)
    {
       /*
-	Don't wase time, don't draw not visible tiles.
-	Also here to draw a map of which doesn't do t full screen.
-       */
+        Don't wase time, don't draw not visible tiles.
+        Also here to draw a map of which doesn't do t full screen.
+      */
       if (visible(*i))
       {
-	 Sprite &tile = tilesets[(*i).tileset];
-	 tile.setTile((*i).tileX, (*i).tileY, tileWidth, tileHeight);
-	 tile.setPos(_x + (*i).x, _y + (*i).y);
-	 
-	 tile.getImage();
-	 tile.Draw();
+         Sprite &tile = tilesets[(*i).tileset];
+         tile.setTile((*i).tileX, (*i).tileY, tileWidth, tileHeight);
+         tile.setPos(_x + (*i).x, _y + (*i).y);
+
+         tile.getImage();
+         tile.Draw();
       }
    }
 }
 
 VALUE GameMap_setPos(VALUE self, VALUE x, VALUE y)
 {
-   GameMap &ref = getRef<GameMap>(self);
-
+   GameMap *ptr = getPtr<GameMap>(self);
    int _x = FIX2INT(x);
    int _y = FIX2INT(y);
 
-   ref.setPos(_x, _y);
+   ptr->setPos(_x, _y);
    return Qnil;
 }
 
 VALUE GameMap_move(VALUE self, VALUE x, VALUE y)
 {
-   GameMap &ref = getRef<GameMap>(self);
+   GameMap *ptr = getPtr<GameMap>(self);
    int _x = FIX2INT(x);
    int _y = FIX2INT(y);
 
-   ref.move(_x, _y);
+   ptr->move(_x, _y);
    return Qnil;
 }
 
 VALUE GameMap_resize(VALUE self, VALUE w, VALUE h)
 {
-   GameMap &ref = getRef<GameMap>(self);
+   GameMap *ptr = getPtr<GameMap>(self);
    int _w = FIX2INT(w);
    int _h = FIX2INT(h);
 
-   ref.resize(_w, _h);
+   ptr->resize(_w, _h);
    return Qnil;
 }
 
 VALUE GameMap_getX(VALUE self)
 {
-   GameMap &ref = getRef<GameMap>(self);
-   return INT2FIX(ref.getX());
+   GameMap *ptr = getPtr<GameMap>(self);
+   return INT2FIX(ptr->getX());
 }
 
 VALUE GameMap_getY(VALUE self)
 {
-   GameMap &ref = getRef<GameMap>(self);
-   return INT2FIX(ref.getY());
+   GameMap *ptr = getPtr<GameMap>(self);
+   return INT2FIX(ptr->getY());
 }
 
 VALUE GameMap_getW(VALUE self)
 {
-   GameMap &ref = getRef<GameMap>(self);
-   return INT2FIX(ref.getW());
+   GameMap *ptr = getPtr<GameMap>(self);
+   return INT2FIX(ptr->getW());
 }
 
 VALUE GameMap_getH(VALUE self)
 {
-   GameMap &ref = getRef<GameMap>(self);
-   return INT2FIX(ref.getH());
+   GameMap *ptr = getPtr<GameMap>(self);
+   return INT2FIX(ptr->getH());
 }
 
 VALUE GameMap_addTileset(VALUE self, VALUE name)
 {
-   GameMap &ref = getRef<GameMap>(self);
-   
-   ref.addTileset(StringValuePtr(name));
+   GameMap *ptr = getPtr<GameMap>(self);
+
+   ptr->addTileset(StringValuePtr(name));
    return Qnil;
 }
 
 VALUE GameMap_setTileSize(VALUE self, VALUE w, VALUE h)
 {
-   GameMap &ref = getRef<GameMap>(self);
+   GameMap *ptr = getPtr<GameMap>(self);
    int _w = FIX2INT(w);
    int _h = FIX2INT(h);
-   
-   ref.setTileSize(_w, _h);
+
+   ptr->setTileSize(_w, _h);
    return Qnil;
 }
 
-VALUE GameMap_addElem(VALUE self, VALUE tileset, VALUE tX, VALUE tY, 
-		      VALUE x, VALUE y)
+VALUE GameMap_addElem(VALUE self, VALUE tileset, VALUE tX, VALUE tY,
+                      VALUE x, VALUE y)
 {
-   GameMap &ref = getRef<GameMap>(self);
+   GameMap *ptr = getPtr<GameMap>(self);
    int _tileset = FIX2INT(tileset);
    int _tX = FIX2INT(tX);
    int _tY = FIX2INT(tY);
    int _x = FIX2INT(x);
    int _y = FIX2INT(y);
 
-   ref.addElem(_tileset, _tX, _tY, _x, _y);
+   ptr->addElem(_tileset, _tX, _tY, _x, _y);
    return Qnil;
 }
 
 VALUE GameMap_collide(VALUE self, VALUE spr)
 {
-   GameMap &ref = getRef<GameMap>(self);
-   Sprite &_spr = getRef<Sprite>(self);
+   GameMap *ptr = getPtr<GameMap>(self);
+   Sprite *_spr = getPtr<Sprite>(spr);
 
-   if (ref.collide(_spr))
+   if (ptr->collide(_spr))
       return Qtrue;
    return Qfalse;
 }
 
 VALUE GameMap_isOn(VALUE self, VALUE x, VALUE y)
 {
-   GameMap &ref = getRef<GameMap>(self);
+   GameMap *ptr = getPtr<GameMap>(self);
    int _x = FIX2INT(x);
    int _y = FIX2INT(y);
 
-   if (ref.isOn(_x, _y))
+   if (ptr->isOn(_x, _y))
       return Qtrue;
    return Qfalse;
 }
 
 VALUE GameMap_clear(VALUE self)
 {
-   GameMap &ref = getRef<GameMap>(self);
-   ref.clear();
+   GameMap *ptr = getPtr<GameMap>(self);
+   ptr->clear();
 
    return Qnil;
 }
 
 VALUE GameMap_draw(VALUE self)
 {
-   GameMap &ref = getRef<GameMap>(self);
-   ref.draw();
+   GameMap *ptr = getPtr<GameMap>(self);
+   ptr->draw();
 
    return Qnil;
 }
--- Joyau/GameMap.hpp
+++ Joyau/GameMap.hpp
@@ -1,18 +1,18 @@
 /*Copyright (C) 2009 Verhetsel Kilian
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.*/
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.*/
 
 #ifndef __JOYAU_MAP__
 #define __JOYAU_MAP__
@@ -39,7 +39,7 @@ public:
 
    void addElem(int tileset, int tX, int tY, int x, int y);
 
-   bool collide(Sprite &spr);
+   bool collide(Sprite *spr);
    bool isOn(int x, int y);
 
    void clear();
@@ -62,7 +62,7 @@ private:
 
    int tileWidth, tileHeight;
 protected:
-   bool visible(Tile &t);
+   bool visible(Tile t);
 };
 
 VALUE GameMap_setPos(VALUE self, VALUE x, VALUE y);
@@ -77,8 +77,8 @@ VALUE GameMap_getH(VALUE self);
 VALUE GameMap_addTileset(VALUE self, VALUE name);
 VALUE GameMap_setTileSize(VALUE self, VALUE w, VALUE h);
 
-VALUE GameMap_addElem(VALUE self, VALUE tileset, VALUE tX, VALUE tY, 
-		      VALUE x, VALUE y);
+VALUE GameMap_addElem(VALUE self, VALUE tileset, VALUE tX, VALUE tY,
+                      VALUE x, VALUE y);
 
 VALUE GameMap_collide(VALUE self, VALUE spr);
 VALUE GameMap_isOn(VALUE self, VALUE x, VALUE y);
