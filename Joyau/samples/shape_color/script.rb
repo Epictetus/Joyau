accbce3f763d7ffa97f661ff92e9741ef218c4c8
Kilian Verhetsel <kilianverhetsel@yahoo.fr>
2009-08-03 14:05:39 +0200

    added a shape sample

---
+++ Joyau/samples/shape_color/script.rb
@@ -0,0 +1,47 @@
+initLib
+initGfx
+
+rect = DrawableRect.new # Of course, instanciate a Shape would have no sense..
+rect.resize(100, 100)
+rect.setColor(255, 0, 0)
+
+skip = false
+
+grad = [ color(255, 0, 0),
+         color(0, 255, 0),
+         color(0, 0, 255),
+         color(255, 255, 255) ]
+
+txt = DrawableText.new
+txt.toggleScripted
+txt.setPos(100, 0)
+
+while mayPlay
+  readKeys
+  if $keys["pressed_up"]
+    rect.setGradient grad
+  elsif $keys["pressed_down"]
+    rect.setColor grad[rand(4)]
+  end
+
+  str = ""
+  rect.getColors.each { |col|
+    str = str + "r: " + col["r"].to_s + " " + 
+    "g: " + col["g"].to_s + " " + 
+    "b: " + col["b"].to_s + "\n"
+  }
+  txt.setText str
+
+  if !skip
+    startDraw
+    clearScreen
+    rect.draw
+    txt.draw
+    endDraw
+  end
+
+  skip = sync
+end
+
+stopGfx
+stopLib
