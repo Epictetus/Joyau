371d2354227090861da6e2260e48b16c2aaa65d1
Kilian Verhetsel <kilianverhetsel@yahoo.fr>
2009-08-17 18:32:35 +0200

    updated the cursor sample. It uses newer things.

--- Joyau/samples/cursor/script.rb
+++ Joyau/samples/cursor/script.rb
@@ -1,36 +1,31 @@
 initLib
 initGfx
 
-bg = Sprite.new
-bg.setPicture("bg.png")
+bg = Sprite.new("bg.png")
 
-cursor = Cursor.new
-cursor.setPicture("cur.png")
+cursor = Cursor.new("cur.png", 40)
 cursor.setPos(240 - cursor.getW / 2, 136 - cursor.getH / 2)
-cursor.setSensibility(40)
 
-no_color = Hash.new
-no_color["r"] = 0
-no_color["g"] = 0
-no_color["b"] = 0
-no_color["a"] = 0
+setTextBackground color(0, 0, 0, 0)
 
-setTextBackground(no_color)
+skip = false
 
 while mayPlay
   cursor.updatePos
 
-  startDraw
+  if !skip
+    startDraw
+    
+    bg.draw
+    cursor.draw
 
-  bg.draw
-  cursor.draw
+    drawText(0, 0, "x : " + cursor.getX.to_s)
+    drawText(0, 10, "y : " + cursor.getY.to_s)
 
-  drawText(0, 0, "x : " + cursor.getX.to_s)
-  drawText(0, 10, "y : " + cursor.getY.to_s)
+    endDraw
+  end
 
-  endDraw
-
-  sync
+  skip = sync
 end
 
 stopGfx
