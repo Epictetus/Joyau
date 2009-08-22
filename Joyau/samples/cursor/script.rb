6f594205ab7a6a514b4d36c1efe48fea974484ef
Kilian Verhetsel <kilianverhetsel@yahoo.fr>
2009-08-22 13:14:58 +0200

    cursor sample updated.

--- Joyau/samples/cursor/script.rb
+++ Joyau/samples/cursor/script.rb
@@ -4,7 +4,7 @@ initGfx
 bg = Sprite.new("bg.png")
 
 cursor = Cursor.new("cur.png", 40)
-cursor.setPos(240 - cursor.getW / 2, 136 - cursor.getH / 2)
+cursor.setPos(240 - cursor.w / 2, 136 - cursor.h / 2)
 
 setTextBackground color(0, 0, 0, 0)
 
@@ -19,8 +19,8 @@ while mayPlay
     bg.draw
     cursor.draw
 
-    drawText(0, 0, "x : " + cursor.getX.to_s)
-    drawText(0, 10, "y : " + cursor.getY.to_s)
+    drawText(0, 0, "x : " + cursor.x.to_s)
+    drawText(0, 10, "y : " + cursor.y.to_s)
 
     endDraw
   end
