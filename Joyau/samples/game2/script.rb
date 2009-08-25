f21bb7c11ee73a87ea56083cfc580181df8d18f0
Kilian Verhetsel <kilianverhetsel@yahoo.fr>
2009-08-25 17:24:33 +0200

    game2 samples updated

--- Joyau/samples/game2/script.rb
+++ Joyau/samples/game2/script.rb
@@ -3,94 +3,83 @@ initGfx
 
 speed = 3
 
-scroll = Scrolling.new
+scroll = Scrolling.new("bg.png")
 scroll.setSprite("bg.png")
-scroll.setPos(0, 0)
-scroll.setSpeed(speed)
+scroll.pos = Point.new(0, 0)
+scroll.speed = speed
 scroll.setDir($directions["LEFT"])
 
 obstacles = []
-for i in 0..4 # having 5 obstacles is great
-  obstacles[i] = Sprite.new
-  obstacles[i].setPicture("obstacle.png")
-  obstacles[i].setPos(rand(480), rand(272))
-end
+5.times {
+  obs = Sprite.new("obstacle.png")
+  obs.pos = Point.new(rand(480), rand(272))
+  obstacles << obs
+}
 
-hero = Sprite.new
-hero.setPicture("angel.png")
+hero = Sprite.new("angel.png")
 hero.setAnim(3, 4)
-hero.setDirection($directions["RIGHT"])
-hero.setPos(480 - hero.getW, 272 - hero.getH)
-
+hero.direction = $directions["RIGHT"]
+hero.pos = Point.new(480 - hero.w, 272 - hero.h)
+ 
 score = 0
 life = 100
-startTimestamp = timestamp
 
-no_color = Hash.new
-no_color["r"] = 0
-no_color["g"] = 0
-no_color["b"] = 0
-no_color["a"] = 0
-
-setTextBackground(no_color)
-
+timer = Timer.new
+ 
 while mayPlay and life > 0
   # We'll let the hero play
   readKeys
   if $keys["down"]
-    if hero.getY + 1 < 272 - hero.getW
+    if hero.y + 1 < 272 - hero.w
       hero.move(0, 1)
     end
   elsif $keys["up"]
-    if hero.getY - 1 > 0
+    if hero.y - 1 > 0
       hero.move(0, -1)
     end
   end
-
+ 
   if $keys["right"]
     hero.move(speed, 0)
   else
     hero.move(-speed * 2, 0)
   end
-
+ 
   # He'll lose life if he isn't in the map :
-  if hero.getX >= 480
+  if hero.x >= 480
     life -= 1
     hero.move(-10, 0)
-  elsif hero.getX <= 0
+  elsif hero.x <= 0
     life -= 1
     hero.move(10, 0)
   end
-
+ 
   # Then, it's the obstacle turn :
-  for i in obstacles
-    i.move(-speed, 0)
-    if i.collide(hero)
+  obstacles.each { |obs|
+    obs.move(-speed, 0)
+    if obs.collide(hero)
       life -= 1
-      hero.move(-i.getW, 0)
+      hero.move(-obs.w, 0)
     end
-    if i.getX < 0 - i.getW
-      i.setPos(480, rand(272))
+    if obs.x < 0 - obs.w
+      obs.pos = Point.new(480, rand(272))
     end
-  end
-
+  }
+ 
   scroll.play
-  score = timestamp - startTimestamp
-
+   
   startDraw
   scroll.draw
-  for i in obstacles
-    i.draw
-  end
+  obstacles.each { |obs| obs.draw }
   hero.draw
-
-  drawText(0, 0, "Score : " + score.to_s)
-  drawText(0, 10, "Life : " + life.to_s)
-
+ 
+  drawScripted(0, 0, "Score : " + timer.time.to_s)
+  drawScripted(0, 10, "Life : " + life.to_s)
+ 
   endDraw
-
+ 
   sync
 end
-
+ 
 stopLib
 stopGfx
