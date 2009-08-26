3ee79ec4b9e9c5d72eddbd2b8e5699c613e69da2
Kilian Verhetsel <kilianverhetsel@yahoo.fr>
2009-08-26 14:34:41 +0200

    added an update method to the pad class

--- Joyau/Keys.cpp
+++ Joyau/Keys.cpp
@@ -307,6 +307,7 @@ void defineKeys()
    defFunc("gets", Joyau_gets, 0);
 
    VALUE cPad = rb_define_class("Pad", rb_cObject);
+   defClassMethod(cPad, "update", Pad_update, 0);
    defClassMethod(cPad, "held?", Pad_held, 1);
    defClassMethod(cPad, "pressed?", Pad_pressed, 1);
    defClassMethod(cPad, "released?", Pad_released, 1);
