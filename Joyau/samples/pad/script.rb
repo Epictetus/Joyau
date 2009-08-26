fef85c7d36db2318d0310d60695bfef81492e847
Kilian Verhetsel <kilianverhetsel@yahoo.fr>
2009-08-26 14:38:17 +0200

    added a sample which shows how to use the Pad class

---
+++ Joyau/samples/pad/script.rb
@@ -0,0 +1,37 @@
+initLib
+initGfx
+
+keys = [ "select", "start", "up", "down", "right", "left", "L", "R", "cross",
+         "triangle", "square", "hold" ]
+
+while mayPlay
+  Pad.update
+
+  heldMsg = ""
+  releasedMsg = ""
+  pressedMsg = ""
+  
+  keys.each { |key|
+    if Pad.held? key
+      heldMsg += key + " is held\n"
+    end
+    if Pad.pressed? key
+      pressedMsg += key + " is pressed\n"
+    end
+    if Pad.released? key
+      releasedMsg += key + " is released\n"
+    end
+  }
+
+  startDraw
+  clearScreen
+  drawScripted(0, 0, heldMsg)
+  drawScripted(100, 0, pressedMsg)
+  drawScripted(200, 0, releasedMsg)
+  endDraw
+  
+  sync
+end
+
+stopGfx
+stopLib
