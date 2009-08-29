fe01523872d006c2310361b631d918258b3e4ce8
Kilian Verhetsel <kilianverhetsel@yahoo.fr>
2009-08-29 19:13:58 +0200

    the audio_spacial sample shows more actions on Vector3f

--- Joyau/samples/audio_spacial/script.rb
+++ Joyau/samples/audio_spacial/script.rb
@@ -2,14 +2,26 @@ initLib
 initGfx
 initAudio
 
+vector =  Vector3f.new(1.0, 1.0, 1.0)
+Listener.pos = Vector3f.new(-1.0, -1.0, -1.0)
+
 music = Stream.new("music.ogg")
-music.pos = Vector3f.new(1.0, 1.0, 1.0)
+music.pos = vector
 skip = false
 
+oldTime = timestamp
+
 while mayPlay and music.update and music.play
+  if oldTime <= timestamp - 3
+    vector.x += 1.0
+    vector.y += 1.0
+    vector.z += 1.0
+    music.pos = vector
+  end
+
   if !skip
     startDraw
-    drawText(0, 131, "This is an audio sample. It should play a music.")
+    drawText(0, 0, "This is an audio sample. It should play a music.")
     endDraw
   end
 
