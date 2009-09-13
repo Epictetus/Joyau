af570552f1f6e00bc30ae8a176aaa6bdcf8ddc93
Kilian Verhetsel <kilianverhetsel@yahoo.fr>
2009-09-13 09:26:53 +0200

    The cursor sample uses cancelMove

--- Joyau/samples/cursor/script.rb
+++ Joyau/samples/cursor/script.rb
@@ -6,17 +6,23 @@ bg = Sprite.new("bg.png")
 cursor = Cursor.new("cur.png", 40)
 cursor.setPos(240 - cursor.w / 2, 136 - cursor.h / 2)
 
+obstacle = Sprite.new("obstacle.png")
+
 setTextBackground color(0, 0, 0, 0)
 
 skip = false
 
 while mayPlay
   cursor.updatePos
+  if obstacle.collide(cursor)
+    cursor.cancelMove
+  end
 
   if !skip
     startDraw
     
     bg.draw
+    obstacle.draw
     cursor.draw
 
     drawText(0, 0, "x : " + cursor.x.to_s)
