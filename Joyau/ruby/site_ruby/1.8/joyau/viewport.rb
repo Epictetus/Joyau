95bb371c35cd7eff951c7589bbcf86994c5c3a38
Kilian Verhetsel <kilianverhetsel@yahoo.fr>
2009-10-12 21:01:01 +0200

    added a viewport class and sample

---
+++ Joyau/ruby/site_ruby/1.8/joyau/viewport.rb
@@ -0,0 +1,56 @@
+#
+# pad.rb
+# Copyright 2009 Verhetsel Kilian
+#
+# This program can be distributed under the terms of the GNU GPL.
+# See the file COPYING.
+#
+
+#
+# When writing  game, we might have to think about hero's position,
+# obstacle's position, ... but this can be confusing.
+#
+# When a viewport is drawed, it's object are moved, drawed, and then, their
+# moves are cancelled. So, you can think about the position in the world without
+# considering the PSP's screen.
+#
+class Viewport < Drawable
+  attr_reader :objects
+
+  #
+  # Creates a new Viewport.
+  #
+  def self.new
+    ret = super
+    ret.initialize
+
+    return ret
+  end
+
+  def initialize
+    @objects = []
+  end
+
+  #
+  # Inserts a new object in the Viewport. This method raises an exception
+  # if the given object is not a Drawable.
+  #
+  def <<(obj)
+    raise TypeError, "obj has to be a Drawable." unless obj.is_a? Drawable
+    @objects << obj
+  end
+
+  #
+  # Draws all the objects in the Viewport.
+  #
+  def draw
+    @objects.each { |i|
+      old_pos = Point.new(i.x, i.y)
+      i.pos = Point.new(x + i.x, y + i.y)
+      i.draw
+      i.pos = old_pos
+    }
+  end
+
+  public :initialize
+end
---
+++ Joyau/samples/viewport/script.rb
@@ -0,0 +1,31 @@
+require 'joyau/viewport.rb'
+
+initLib
+initGfx
+
+spr = Sprite.new("pic.png")
+viewport = Viewport.new
+
+viewport << spr
+
+while mayPlay
+  Pad.update
+
+  if Pad.held? Pad::LEFT
+    viewport.move(6, 0)
+  elsif Pad.held? Pad::RIGHT
+    viewport.move(-6, 0)
+  end
+
+  spr.move(1, 0)
+
+  startDraw
+  clearScreen
+  viewport.draw
+  endDraw
+
+  sync
+end
+
+stopGfx
+stopLib
