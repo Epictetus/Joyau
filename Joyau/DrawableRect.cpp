ac486aed80914a37c8f42508b18fbe930e2378b2
Kilian Verhetsel <kilianverhetsel@yahoo.fr>
2009-10-19 19:12:32 +0200

    updated DrawableRect

--- Joyau/DrawableRect.cpp
+++ Joyau/DrawableRect.cpp
@@ -1,4 +1,4 @@
-x/*Copyright (C) 2009 Verhetsel Kilian
+/*Copyright (C) 2009 Verhetsel Kilian
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
